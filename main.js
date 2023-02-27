bd1f680d061b65da828f7c4bf0051cf376093123
Dannycyph3r <designmatr1x0@gmail.com>
2023-02-27 09:14:31 -0500

    mo

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+
 import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/yuO5TfYtFRvMJEzX/scene.splinecode');
+
+
+LottieInteractivity.create({
+    mode: 'scroll',
+    player: '#firstLottie',
+    actions: [
+        {
+          visibility: [0,1],
+          type: 'seek',
+          frames: [0, 261],
+        },
+      ],
+  })
+
+  LottieInteractivity.create({
+    player:'#tenthLottie',
+    mode:"cursor",
+    actions: [
+        {
+            type: "click",
+            forceFlag: false
+        }
+    ]
+});
